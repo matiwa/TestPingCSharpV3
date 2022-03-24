5b065dbc7d5cd0a007f018c43b612fe25d719250
matiwa <mateuszivan1994@wp.pl>
2022-03-24 16:36:10 +0100

    first commit

---
+++ TestPingCSharpV3/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace TestPingCSharpV3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.BPing = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BAdd = new System.Windows.Forms.Button();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.ListBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Address:";
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(67, 10);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(170, 20);
+            this.TextBox1.TabIndex = 1;
+            this.TextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            // 
+            // BPing
+            // 
+            this.BPing.Location = new System.Drawing.Point(243, 8);
+            this.BPing.Name = "BPing";
+            this.BPing.Size = new System.Drawing.Size(75, 23);
+            this.BPing.TabIndex = 2;
+            this.BPing.Text = "Ping";
+            this.BPing.UseVisualStyleBackColor = true;
+            this.BPing.Click += new System.EventHandler(this.BPing_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(306, 251);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Repeat:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(50, 298);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(62, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(145, 298);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(62, 20);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "TTL:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BAdd);
+            this.groupBox1.Controls.Add(this.TextBox2);
+            this.groupBox1.Controls.Add(this.ListBox2);
+            this.groupBox1.Location = new System.Drawing.Point(324, 8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 310);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Host list:";
+            // 
+            // BAdd
+            // 
+            this.BAdd.Location = new System.Drawing.Point(6, 282);
+            this.BAdd.Name = "BAdd";
+            this.BAdd.Size = new System.Drawing.Size(75, 23);
+            this.BAdd.TabIndex = 9;
+            this.BAdd.Text = "Add";
+            this.BAdd.UseVisualStyleBackColor = true;
+            this.BAdd.Click += new System.EventHandler(this.BAdd_Click);
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(6, 259);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(188, 20);
+            this.TextBox2.TabIndex = 10;
+            this.TextBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox2_KeyPress);
+            // 
+            // ListBox2
+            // 
+            this.ListBox2.FormattingEnabled = true;
+            this.ListBox2.Location = new System.Drawing.Point(6, 19);
+            this.ListBox2.Name = "ListBox2";
+            this.ListBox2.Size = new System.Drawing.Size(188, 225);
+            this.ListBox2.TabIndex = 9;
+            this.ListBox2.DoubleClick += new System.EventHandler(this.ListBox2_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 332);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BPing);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TestPingCSharpV3";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.Button BPing;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BAdd;
+        private System.Windows.Forms.TextBox TextBox2;
+        private System.Windows.Forms.ListBox ListBox2;
+    }
+}
+
---
+++ TestPingCSharpV3/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace TestPingCSharpV3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BPing_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (TextBox1.Text != "" || ListBox2.Items.Count > 0)
+            {
+                PingOptions opcje = new PingOptions
+                {
+                    Ttl = (int)numericUpDown2.Value,
+                    DontFragment = true
+                };
+                string dane = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+                byte[] bufor = Encoding.ASCII.GetBytes(dane);
+                int timeout = 120;
+                if (TextBox1.Text != "")
+                {
+                    for (int i = 0; i < (int)numericUpDown1.Value; i++)
+                        listBox1.Items.Add(this.WyslijPing(TextBox1.Text, timeout, bufor, opcje));
+                    listBox1.Items.Add("----------------");
+                }
+                if (ListBox2.Items.Count > 0)
+                {
+                    foreach (string host in ListBox2.Items)
+                    {
+                        for (int i = 0; i < (int)numericUpDown1.Value; i++)
+                            listBox1.Items.Add(this.WyslijPing(host, timeout, bufor, opcje));
+                        listBox1.Items.Add("----------------");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No addresses have been entered", "Error");
+            }
+        }
+
+        private void BAdd_Click(object sender, EventArgs e)
+        {
+            if (TextBox2.Text != String.Empty)
+                if (TextBox2.Text.Trim().Length > 0)
+                {
+                    ListBox2.Items.Add(TextBox2.Text);
+                    TextBox1.Clear();
+                }
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) BPing_Click(sender, e);
+        }
+
+        private void TextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) BAdd_Click(sender, e);
+        }
+
+        private void ListBox2_DoubleClick(object sender, EventArgs e)
+        {
+            if (ListBox2.SelectedIndex != -1)
+                ListBox2.Items.RemoveAt(ListBox2.SelectedIndex);
+        }
+
+        private string WyslijPing(string adres, int timeout, byte[] bufor, PingOptions opcje)
+        {
+            Ping ping = new Ping();
+            try
+            {
+                PingReply odpowiedz = ping.Send(adres, timeout, bufor, opcje);
+                if (odpowiedz.Status == IPStatus.Success)
+                    return "Answer from " + adres + " bytes=" +
+                    odpowiedz.Buffer.Length + " time=" + odpowiedz.RoundtripTime + "ms TTL=" +
+                    odpowiedz.Options.Ttl;
+                else
+                    return "Error:" + adres + " " + odpowiedz.Status.ToString();
+            }
+            catch (Exception ex)
+            {
+                return "Error:" + adres + " " + ex.Message;
+            }
+        }
+    }
+}
